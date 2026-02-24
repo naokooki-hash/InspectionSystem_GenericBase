ecbf4ea7a5298c2828d047c603af3cdd17ec4314
okura arrowtrust <n.okura@arrowtrust.jp>
2026-02-24 15:26:49 +0900

    プロジェクトファイルを追加。

---
+++ 20260224SolderInspec/Form1.cs
@@ -0,0 +1,10 @@
+namespace _20260224SolderInspec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 20260224SolderInspec/Program.cs
@@ -0,0 +1,19 @@
+namespace _20260224SolderInspec
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppContext.SetSwitch("System.Drawing.EnableUnixSupport", true);
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
